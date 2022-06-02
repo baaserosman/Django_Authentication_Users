fe2871d5e2f8bab88f19fa7a994585e580a30d00
baaserosman <baaser.osman@gmail.com>
2022-06-02 10:19:10 +0200

    register serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,23 @@
-from rest_framework import serializers
+import re
+from rest_framework import serializers, validators
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
-
+        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.Charfield(
+        required=True,
+        validators = [validate_password],
+        style ={"input_type": "password"}
+    )
+    password2 = serializers.Charfield(
+        required=True,
+        validators = [validate_password],
+        style ={"input_type": "password"}
     )
 
     class Meta:
@@ -23,3 +36,17 @@ class RegisterSerializer(serializers.ModelSerializer):
             "password2" : {"write_only" : True}
 
         }
+    def create(self, validated_data) :
+        password = validated_data.get("password")
+        validated_data.pop("password2")
+        user=User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def validate(self, data) :
+        if data["password"] != data["password2"] :
+            raise serializers.ValidationError(
+                {"message" : "Password fields didn't match!"}
+            )
+        return data
