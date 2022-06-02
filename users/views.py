612fda889fafeadf2f5877920a56ec0ba3116266
baaserosman <baaser.osman@gmail.com>
2022-06-02 10:58:15 +0200

    view created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterAPIs(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exceptions=True)
+        serializer.save()
+        return Response(
+            {
+                "messega" : "User created successfully."
+            }
+        )
